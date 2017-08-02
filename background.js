d6d99b1cb56d89017a94313f1dc12c290a0f94f0
isudzumi <isudzumi@gmail.com>
2017-08-02 19:46:32 +0900

    Version up
    Enable to call alert when success in inserting events to Google Calendar
    Call alert only once regardless of the number of events
    And some changes

--- background.js
+++ background.js
@@ -1,33 +1,43 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  const URL = 'https://www.googleapis.com/calendar/v3/calendars/primary/events?sendNotifications=true';
-	chrome.identity.getAuthToken({ 'interactive' : true }, function(token){
+chrome.runtime.onMessage.addListener(function(request){
+    chrome.identity.getAuthToken({ 'interactive' : true }, function(token){
 
-    if(chrome.runtime.lastError){
-      console.log(chrome.runtime.lastError);
-      return;
-    }
-
-		$.ajax({
-			type: 'POST',
-			url : URL,
-			timeout: 10000,
-			data: request,
-      dataType: 'json',
-      headers: {
-        "Authorization": "Bearer " + token
-      },
-      contentType: 'application/json'
-		}).done(function(data){
-			console.log(data);
-		}).fail(function(xhr, status){
-      if(xhr.status === 401) {
-        chrome.identity.removeCachedAuthToken(
-          {'token' : token}
-        );
+      if(chrome.runtime.lastError){
+        showStatus(chrome.runtime.lastError);
+      } else {
+        insertToCalendar(token, request, showStatus);
       }
-			console.log(xhr);
-			console.log(status);
-		});
-	});
-  }
-);
+    });
+});
+
+function insertToCalendar(token, request, callback){
+  const URL = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
+  $.ajax({
+    type: 'POST',
+    url : URL,
+    timeout: 10000,
+    data: request,
+    dataType: 'json',
+    headers: {
+      "Authorization": "Bearer " + token
+    },
+    contentType: 'application/json'
+  }).done(function(){
+    callback('success');
+  }).fail(function(xhr){
+    if(xhr.status === 401) {
+      chrome.identity.removeCachedAuthToken(
+        {'token' : token}
+      );
+    }
+    callback(xhr.status);
+  });
+}
+
+function showStatus(status){
+  let response = {
+    message : status
+  };
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.sendRequest(tab.id, JSON.stringify(response));
+  })
+}
--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-﻿$(function(){
+class StatusAlert {
+	constructor() {
+		this.count = 0;
+	}
+	counter(){
+		this.count++;
+	}
+	success(){
+		this.counter();
+		this.message = 'カレンダーに登録しました';
+	}
+	fail(text){
+		this.counter();
+		this.message = 'ERROR : ' + text;
+	}
+}
+
+let count = new StatusAlert();
+
+$(function(){
+
 	if(location.pathname == "/emp/project/project_shift.php") {
 		//set button to the page.
 		$("div").append('<div style="margin:0 5%"><div align="left"><button>Googleカレンダーに追加</button></div></div>');
@@ -9,10 +29,13 @@
 });
 
 function getLinks() {
+	let size = 0;
 	$("table:eq(1) tr:gt(1)").find("td:eq(2) > a").each(function(){
 		let shiftId = $(this).attr("href").substring(23, 57);
 		getData(shiftId);
+		size++;
 	});
+	count.size = size;
 }
 
 function getData(link) {
@@ -48,23 +71,14 @@ function getJobDescription(data) {
 	chrome.runtime.sendMessage(JSON.stringify(request));
 }
 
-/**
-function insCal(request){
-	const URL = "https://www.googleapis.com/calendar/v3/calendars/calendarId/events";
-	chrome.identity.getAuthToken({ "interactive" : true }, function(token){
-		console.log(token);
-		$.ajax({
-			type: "POST",
-			url : URL,
-			timeout: 10000,
-			data: request,
-		}).done(function(data){
-			console.log(data);
-		}).fail(function(xhr, status, err){
-			console.log(xhr);
-			console.log(status);
-			console.log(err);
-		});
-	});
-}
-*/
+chrome.extension.onRequest.addListener(function(response){
+	response = JSON.parse(response);
+	if(response.message === 'success') {
+		count.success();
+	} else {
+		count.fail(response.message);
+	}
+	if(count.count == count.size){
+		alert( count.message );
+	}
+})
